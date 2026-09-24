80e7c355dfd57ef032402a6129f0db68a417f350
Sweetnes <999999zqq@gmail.com>
2026-09-25 04:50:20 +0700

    создал приложения users, materials. создали модел пользователя, Модели курса и урока. Регистрированы модели в админке

---
+++ materials/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from materials.models import Course, Lesson
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    """Админ-панель для курсов."""
+    list_display = ("title", "description")
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    """Админ-панель для уроков."""
+    list_display = ("title", "course", "video_url")
---
+++ materials/views.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets, generics
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """ViewSet для CRUD-операций над курсами."""
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonListCreateView(generics.ListCreateAPIView):
+    """Generic-класс для получения списка уроков и создания нового урока."""
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """Generic-класс для получения, обновления и удаления конкретного урока."""
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
